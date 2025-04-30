17c2964443a52a87a5c5b526ca251c397058b400
e19166 <e19166@eng.pdn.ac.lk>
2025-04-30 11:23:54 +0530

    Move main.py to code directory

--- code/auto_updater/prometheus_client.py
+++ code/auto_updater/prometheus_client.py
@@ -1,5 +1,5 @@
 import requests
-from .config import PROMETHEUS_URL, PROMETHEUS_QUERY_CPU
+from .config import PROMETHEUS_URL
 
 def query_prometheus(query):
     response = requests.get(f"{PROMETHEUS_URL}/api/v1/query", params={"query": query})
