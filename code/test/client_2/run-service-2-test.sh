b5bfe2e2650bf372dbd47fc91ede41bbb31a9297
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2025-05-04 18:38:07 +0530

    Update output files

--- code/test/client_2/run-service-2-test.sh
+++ code/test/client_2/run-service-2-test.sh
@@ -12,7 +12,7 @@ REPORT_DIR="$REPORTS_DIR/report_$TIMESTAMP"
 mkdir -p "$REPORT_DIR"
 
 # Run the JMeter test in non-GUI mode
-jmeter -n -t service-2-test.jmx -l "$REPORT_DIR/results2.jtl" -e -o "$REPORT_DIR"
+jmeter -n -t service-2-test.jmx -l "$REPORT_DIR/results2.csv" -e -o "$REPORT_DIR"
 
 # Print a message when the test is completed
-echo "Test completed. Results saved to $REPORT_DIR/results2.jtl and HTML report generated in $REPORT_DIR/"
+echo "Test completed. Results saved to $REPORT_DIR/results2.csv and HTML report generated in $REPORT_DIR/"
