a730ae8ad96467f8eeb166f142a5e463dc56b3d4
e19166 <e19166@eng.pdn.ac.lk>
2025-05-06 14:08:20 +0530

    Add IP Geo service

---
+++ code/services/ip_geo_service/app/geo_utils.py
@@ -0,0 +1,16 @@
+import ip2location
+
+# Load local DB
+db = ip2location.IP2Location("IP2LOCATION-LITE-DB1.BIN")
+
+def get_geolocation(ip: str):
+    try:
+        record = db.get_all(ip)
+        return {
+            "ip": ip,
+            "country": record.country_short,
+            "region": record.region,
+            "city": record.city
+        }
+    except Exception as e:
+        return {"error": str(e)}
---
+++ code/services/ip_geo_service/app/metrics.py
@@ -0,0 +1,7 @@
+from prometheus_client import start_http_server, Summary, Counter
+
+REQUEST_LATENCY = Summary("request_latency_seconds", "Request latency", ["path"])
+REQUEST_COUNT = Counter("request_count_total", "Total number of requests", ["path"])
+
+def start_metrics_server():
+    start_http_server(8001)  # Prometheus will scrape here
