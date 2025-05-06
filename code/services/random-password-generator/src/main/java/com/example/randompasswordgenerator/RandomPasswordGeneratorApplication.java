2796f51ee51b06e2bc281526f620232758e974ca
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2025-05-06 17:40:41 +0530

    Add Random Password Generator service

---
+++ code/services/random-password-generator/src/main/java/com/example/randompasswordgenerator/RandomPasswordGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.randompasswordgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPasswordGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomPasswordGeneratorApplication.class, args);
+	}
+
+}
