2356d13069d37eda90f238d970a51d6cbef57c37
e19166 <e19166@eng.pdn.ac.lk>
2025-05-31 19:50:23 +0530

    Add update method for ema class

--- code/dare_predictor/trend_learner/models.py
+++ code/dare_predictor/trend_learner/models.py
@@ -19,3 +19,10 @@ class EMA:
     def __init__(self, alpha=0.3):
         self.alpha = alpha
         self.last = None
+        
+    def update(self, value):
+        if self.last is None:
+            self.last = value
+        else:
+            self.last = self.alpha * value + (1 - self.alpha) * self.last
+        return self.last
