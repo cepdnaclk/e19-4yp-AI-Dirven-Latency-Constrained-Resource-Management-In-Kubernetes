293ebd6de6b5d7306643d00831399967e568b6b7
e19166 <e19166@eng.pdn.ac.lk>
2025-05-31 19:37:04 +0530

    Add predict method for online linear regressor

--- code/dare_predictor/trend_learner/models.py
+++ code/dare_predictor/trend_learner/models.py
@@ -4,3 +4,7 @@ class OnlineLinearRegressor:
     def __init__(self, learning_rate=0.001):
         self.weights = np.zeros(3)  # [bias, req_rate, time]
         self.learning_rate = learning_rate
+        
+    def predict(self, req_rate, t):
+        x = np.array([1, req_rate, t])
+        return np.dot(self.weights, x)
