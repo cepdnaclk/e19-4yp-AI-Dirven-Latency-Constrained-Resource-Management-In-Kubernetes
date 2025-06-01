997137a4c90e5e32b146c942eb56bd1ee4b612a5
e19166 <e19166@eng.pdn.ac.lk>
2025-06-01 09:52:32 +0530

    Add apply ema method to trend learner class

--- code/dare_tl/dare_tl/model.py
+++ code/dare_tl/dare_tl/model.py
@@ -8,3 +8,10 @@ class TrendLearner:
         self.mem_model = SGDRegressor(max_iter=1000, tol=1e-3)
         self.alpha = alpha
         self.ema_feature = None  # For storing EMA of inputs
+        
+    def _apply_ema(self, X):
+        if self.ema_feature is None:
+            self.ema_feature = X.copy()
+        else:
+            self.ema_feature = self.alpha * X + (1 - self.alpha) * self.ema_feature
+        return self.ema_feature
