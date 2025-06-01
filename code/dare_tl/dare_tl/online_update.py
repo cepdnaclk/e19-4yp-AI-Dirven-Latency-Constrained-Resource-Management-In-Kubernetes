dc0d8783dc83bee7f81798e5611e10c75532f8d8
e19166 <e19166@eng.pdn.ac.lk>
2025-06-01 10:06:41 +0530

    Add online training method

---
+++ code/dare_tl/dare_tl/online_update.py
@@ -0,0 +1,19 @@
+from dare_tl.model import TrendLearner
+import numpy as np
+
+def update_model_online(model_path, new_data_point):
+    model = TrendLearner.load(model_path)
+
+    X = np.array([[
+        new_data_point["CPU_Limit"],
+        new_data_point["Memory_Limit"],
+        new_data_point["CPU_Usage"],
+        new_data_point["Memory_Usage"],
+        new_data_point["RequestRate"]
+    ]])
+
+    y_cpu = np.array([new_data_point["CPU_Usage_Delta"]])
+    y_mem = np.array([new_data_point["Memory_Usage_Delta"]])
+
+    model.partial_fit(X, y_cpu, y_mem)
+    model.save(model_path)
