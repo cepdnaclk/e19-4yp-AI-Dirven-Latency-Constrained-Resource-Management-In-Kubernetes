846f2ccaebb05f31a5ac5d7d1607d6fb5e075787
e19166 <e19166@eng.pdn.ac.lk>
2025-06-02 11:39:46 +0530

    prepare a test dataset

--- code/dare_tl/dare_tl/evaluation.py
+++ code/dare_tl/dare_tl/evaluation.py
@@ -1,9 +1,15 @@
 from model import TrendLearner
 import joblib
 import numpy as np
+import pandas as pd
 
 model_path = "./models/tl_model.pkl"
 scaler_path = "./models/scaler.pkl"
 
 model = TrendLearner.load(model_path)
 scaler = joblib.load(scaler_path)
+
+df = pd.read_csv("./data/resource_usage.csv")
+X_raw = df[["CPU_Usage", "Memory_Usage", "RequestRate", "CPU_Limit", "Memory_Limit"]].values
+y_cpu = df["CPU_Usage_Delta"].values
+y_mem = df["Memory_Usage_Delta"].values
